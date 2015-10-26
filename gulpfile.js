2d6a7d97316ca112d288db8534dc914958f96101
Alex Filatau <filatau@gmail.com>
2015-10-25 20:51:39 -0700

    Simplify gulp sequence for psi task and add list of tasks for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var ngrok = require('ngrok');
 var psi = require('psi');
 var sequence = require('run-sequence');
 var browserSync = require('browser-sync');
+var taskListing = require('gulp-task-listing');
+
 var site = '';
 var portVal = 3020;
 
@@ -41,24 +43,12 @@ gulp.task('browser-sync-psi', function(cb) {
 });
 
 gulp.task('psi-seq', function(cb) {
-  return sequence(
+  sequence(
     'browser-sync-psi',
     'ngrok-url',
     'psi-desktop',
     'psi-mobile',
-    function(err) {
-      //if any error happened in the previous tasks, exit with a code > 0
-      if (err) {
-        var exitCode = 2;
-        console.log('[ERROR] gulp build task failed', err);
-        console.log('[FAIL] gulp build task failed - exiting with code ' + exitCode);
-        // return process.exit(exitCode);
-        return cb(); // continue
-      }
-      else {
-        return cb();
-      }
-    }
+    cb
   );
 });
 
@@ -66,6 +56,8 @@ gulp.task('psi', ['psi-seq'], function() {
   console.log('Woohoo! Check out your page speed scores!');
   process.exit();
 });
+
+gulp.task('help', taskListing);
 
 gulp.task('default', function() {
   console.log("Gulp ready");
